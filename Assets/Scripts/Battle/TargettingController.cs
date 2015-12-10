ba13a4ae272e3633668a8229a77dcc2b0a0145df
britg <brit@britg.com>
2015-12-10 10:56:26 -0600

    Set up a targetting FSM

---
+++ Assets/Scripts/Battle/TargettingController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargettingController : BattleFrameBehaviour {
+	
+	public bool isControllable = false;
+	
+	void Awake () {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isControllable = (character.currentBattleSide == Battle.Side.Friend);
+	}
+	
+	public void FSM_Handler_Target (GameObject targetObj) {
+		Debug.Log("Targetted " + targetObj);
+	}
+}
