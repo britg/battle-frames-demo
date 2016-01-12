0f79876c028d05342b06ecd019c4488560f5362f
britg <brit@britg.com>
2016-01-12 14:40:53 -0600

    Initial implementation of websockets

---
+++ Assets/Scripts/Battle/BattleApiController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+
+public class BattleApiController : APIBehaviour {
+    
+    public WebsocketsStateMachine stateMachine;
+    
+    WebSocket ws;
+
+	// Use this for initialization
+	void Start() {
+        ws = new WebSocket("ws://echo.websocket.org");
+
+        ws.OnOpen += OnOpenHandler;
+        ws.OnMessage += OnMessageHandler;
+        ws.OnClose += OnCloseHandler;
+
+        stateMachine.AddHandler(State.Running, () => {
+            new tpd.Wait(this, 3, () => {
+                ws.ConnectAsync();
+            });
+        });
+
+        stateMachine.AddHandler(State.Connected, () => {
+            stateMachine.Transition(State.Ping);
+        });
+
+        stateMachine.AddHandler(State.Ping, () => {
+            new tpd.Wait(this, 3, () => {
+               ws.SendAsync("This WebSockets stuff is a breeze!", OnSendComplete);
+            });
+        });
+
+        stateMachine.AddHandler(State.Pong, () => {
+            new tpd.Wait(this, 3, () => {
+                ws.CloseAsync();
+            });
+        });
+
+        stateMachine.Run();
+    }
+
+    private void OnOpenHandler(object sender, System.EventArgs e) {
+        Debug.Log("WebSocket connected!");
+        stateMachine.Transition(State.Connected);
+    }
+
+    private void OnMessageHandler(object sender, MessageEventArgs e) {
+        Debug.Log("WebSocket server said: " + e.Data);
+        stateMachine.Transition(State.Pong);
+    }
+
+    private void OnCloseHandler(object sender, CloseEventArgs e) {
+        Debug.Log("WebSocket closed with reason: " + e.Reason);
+        stateMachine.Transition(State.Done);
+    }
+
+    private void OnSendComplete(bool success) {
+        Debug.Log("Message sent successfully? " + success);
+    }
+}
--- Assets/Scripts/Lib/tpd.cs
+++ Assets/Scripts/Lib/tpd.cs
@@ -156,12 +156,22 @@ public class tpd {
    *    WWW
    */
    
-   public void DebugResponseHeaders (WWW response) {
+   public static void DebugResponseHeaders (WWW response) {
         foreach(KeyValuePair<string, string> entry in response.responseHeaders) {
             Debug.Log(entry.Key + " = " + entry.Value);
         }
    }
 
+    public class Wait {
+        public Wait(MonoBehaviour mb, float seconds, Action a) {
+            mb.StartCoroutine(RunAndWait(seconds, a));
+        }
+
+        IEnumerator RunAndWait(float seconds, Action a) {
+            yield return new WaitForSeconds(seconds);
+            a();
+        }
+    }
 }
 
 public class Notification {
